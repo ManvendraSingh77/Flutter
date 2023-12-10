177a21778bd657e30077d25d1fcd3ba98eeb5594
ManvendraSingh77 <msgithubrepo@gmail.com>
2023-12-10 19:00:41 +0530

    namecardapp

---
+++ name_card_app/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MiCardApp());
+}
+
+class MiCardApp extends StatelessWidget {
+  const MiCardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/image.jpg'),
+              ),
+              Text(
+                'Hancock',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 40.0,
+                ),
+              ),
+              Text(
+                'ANIME QUEEN',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 15.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFC5F5F0),
+                ),
+              ),
+              SizedBox(
+                width: 150,
+                height: 15,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 35),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+11 22 33445566',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.teal,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 35.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'angela@email.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
